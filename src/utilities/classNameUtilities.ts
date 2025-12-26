81ac0c1fe606a4e76b941237603525f115a1e0cc
Gijs Nieuwenhuis <gijsnieuwenhuis@gmail.com>
2025-12-26 08:30:15 +0100

    Added some basic documentation on composeClassnames utility

--- src/utilities/classNameUtilities.ts
+++ src/utilities/classNameUtilities.ts
@@ -1,6 +1,9 @@
 import classnames from 'classnames';
 import { twMerge } from 'tailwind-merge';
 
+/**
+ * Alloes logic in class name compositions, using the `classnames` utility, but also ensures that, when using Tailwind CSS class names, the last class name replaces the previous one. I.e. `text-black text-white` will end up being `text-white`.
+ */
 export const composeClassnames = (
     ...args: classnames.ArgumentArray
 ): string => {
