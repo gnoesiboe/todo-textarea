315a04ac666ba849412ef6ddf4c135129da7b3ef
Gijs Nieuwenhuis <gijsnieuwenhuis@gmail.com>
2025-12-28 07:15:23 +0100

    Add support for base64 encoding UTF characters

--- src/context/utilities/base64utilities.ts
+++ src/context/utilities/base64utilities.ts
@@ -1,7 +1,17 @@
 export function encodeBase64(text: string): string {
-    return btoa(text);
+    const bytes = new TextEncoder().encode(text);
+
+    const binaryString = Array.from(bytes, (byte) =>
+        String.fromCodePoint(byte),
+    ).join('');
+
+    return btoa(binaryString);
 }
 
 export function decodeBase64(encodedText: string): string {
-    return atob(encodedText);
+    const binString = atob(encodedText);
+
+    const bytes = Uint8Array.from(binString, (m) => m.codePointAt(0) as number);
+
+    return new TextDecoder().decode(bytes);
 }
