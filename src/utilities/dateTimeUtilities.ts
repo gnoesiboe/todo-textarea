4f91c4e191af6f6b163d66edfcb4097d658af414
Gijs Nieuwenhuis <gijsnieuwenhuis@gmail.com>
2026-01-05 21:46:50 +0100

    Removed space in done datetime formatting

--- src/utilities/dateTimeUtilities.ts
+++ src/utilities/dateTimeUtilities.ts
@@ -1,7 +1,7 @@
 import { format } from 'date-fns';
 
 export function formatAsDateTime(date: Date): string {
-    return format(date, 'yyyy-MM-dd @ HH:mm') + 'u';
+    return format(date, 'yyyy-MM-dd@HH:mm') + 'u';
 }
 
 export function formatAsTime(date: Date): string {
